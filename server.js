da1497666c688e001735b83bb433fc185bfc95a5
Hiren Reshamwala <hiren.reshamwala@gmail.com>
2021-05-08 11:46:59 +0530

    Add express server

--- server.js
+++ server.js
@@ -1,33 +1,16 @@
-/* ====== create node.js server with core 'http' module ====== */
-// dependencies
-const http = require("http");
+const express = require('express')
+const app = express()
+const port = 3000
 
-// PORT
-const PORT = 3000;
 
-// server create
-const server = http.createServer((req, res) => {
-   if (req.url === "/") {
-      res.write("This is home page.");
-      res.end();
-   } else if (req.url === "/about" && req.method === "GET") {
-      res.write("This is about page.");
-      res.end();
-   } else {
-      res.write("Not Found!");
-      res.end();
-   }
-});
+app.get('/', (req, res) => {
+   res.send('Hello World!')
+})
 
-// server listen port
-server.listen(PORT);
+app.get('/about', (req, res) => {
+   res.send({
+      demo: 'test demo'
+   })
+})
 
-console.log("Starting server");
-console.log(`Server is running on PORT: ${PORT}`);
-
-// ======== Instructions ========
-// save this as index.js
-// you have to download and install node.js on your machine
-// open terminal or command prompt
-// type node index.js
-// find your server at http://localhost:3000
+app.listen(port, () => console.log(`Example app listening at ${port}`))
